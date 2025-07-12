9e98b5aaf9b62ae2d1607c944a2e1408f578f238
Jordan Dominion <Cyberboss@users.noreply.github.com>
2025-07-11 20:29:22 -0400

    WIP

---
+++ src/byond/mod.rs
@@ -0,0 +1,197 @@
+use std::{mem::transmute, ops::Deref};
+
+use crate::byond::offsets::Offsets;
+
+pub mod offsets;
+
+pub type BuildNumber = i32;
+
+#[repr(C)]
+union ObjectPart1 {
+    padding: u32,
+    object_type: u8,
+}
+
+#[repr(C)]
+union ObjectPart2 {
+    i: u32,
+    f: f32,
+}
+
+#[repr(C)]
+struct DMObject {
+    part_1: ObjectPart1,
+    part_2: ObjectPart2,
+}
+
+#[repr(C)]
+struct String {
+    data: *const u8,
+    id: u32,
+    left: *const String,
+    right: *const String,
+    refcount: u32,
+    unknown_0: u32,
+    length: u32,
+}
+
+#[repr(C)]
+struct ProcDefinition {
+    path: u32,
+    name: u32,
+    desc: u32,
+    category: u32,
+    flags: u32,
+    _unknown0: u32,
+    bytecode: u32,
+    locals: u32,
+    parameters: u32,
+}
+
+#[repr(C)]
+struct Bytecode {
+    length: u16,
+    unknown_0: u32,
+    bytecode: *const u32,
+}
+
+#[repr(C)]
+struct Locals {
+    length: u16,
+    unknown_0: u32,
+    locals: *const u32,
+}
+
+#[repr(C)]
+struct Params {
+    length: u16,
+    unknown_0: u32,
+    params: *const u32,
+}
+
+#[repr(C)]
+struct Misc {
+    bytecode: Bytecode,
+    locals: Locals,
+    params: Params,
+}
+
+#[repr(C)]
+struct ExecutionContext;
+
+#[repr(C)]
+struct Proc {
+    definition: u32,
+    flags: u8,
+    supers: u8,
+    unused: u16,
+    usr: DMObject,
+    src: DMObject,
+    context: *const ExecutionContext,
+    sequence: u32,
+    callback: fn(DMObject, u32) -> (),
+    callback_arg: u32,
+    argc: u32,
+    argv: *const [DMObject],
+    unknown_0: u32,
+}
+
+#[repr(C)]
+struct ProcDefsDescriptor {
+    size: usize,
+    path_offset: usize,
+    bytecode_offset: usize,
+}
+
+#[repr(C)]
+struct Trampoline {
+    exec_proc: [u8; 32],
+    server_tick: [u8; 32],
+    send_maps: [u8; 32],
+}
+
+#[cfg(target_os = "windows")]
+type ExecProcFunction = unsafe extern "cdecl" fn(*const Proc) -> DMObject;
+
+#[cfg(not(target_os = "windows"))]
+type ExecProcFunction = unsafe extern "regparm3" fn(*const Proc) -> DMObject;
+
+#[cfg(target_os = "windows")]
+type ServerTickFunction = unsafe extern "stdcall" fn() -> i32;
+
+#[cfg(not(target_os = "windows"))]
+type ServerTickFunction = unsafe extern "cdecl" fn() -> i32;
+
+type SendMapsFunction = unsafe extern "cdecl" fn();
+
+struct SizedArray<T> {
+    items: *const T,
+    items_len: *const usize,
+}
+
+impl<T> Deref for SizedArray<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        todo!()
+    }
+}
+
+struct ProcdefPointer(usize);
+
+struct ByondReflectionData {
+    strings_base_address: *const String,
+    strings_len: *const usize,
+    miscs_base_address: *const Misc,
+    miscs_len: *const usize,
+    procdefs_base_address: usize,
+    procdefs_len: *const usize,
+    procdef_desc: ProcDefsDescriptor,
+    exec_proc_address: usize,
+    orig_exec_proc: ExecProcFunction,
+    server_tick_address: usize,
+    orig_server_tick: ServerTickFunction,
+    send_maps_address: usize,
+    orig_send_maps: SendMapsFunction,
+    trampoline: Trampoline,
+}
+
+impl ByondReflectionData {
+    fn create_and_initialize_hooks(offsets: &Offsets, byondcore_base_address: usize) -> Self {
+        let prologues = [
+            offsets.prologue >> 0,
+            offsets.prologue >> 8,
+            offsets.prologue >> 16,
+        ];
+        unsafe {
+            Self {
+                strings_base_address: transmute(byondcore_base_address + offsets.strings),
+                strings_len: transmute(byondcore_base_address + offsets.strings_len),
+                miscs_base_address: transmute(byondcore_base_address + offsets.miscs),
+                miscs_len: transmute(byondcore_base_address + offsets.miscs_len),
+                procdefs_base_address: byondcore_base_address + offsets.procdefs,
+                procdefs_len: transmute(byondcore_base_address + offsets.procdefs_len),
+                procdef_desc: ProcDefsDescriptor {
+                    size: (offsets.procdefs_descriptor >> 0) & 0xFF,
+                    path_offset: (offsets.procdefs_descriptor >> 8) & 0xFF,
+                    bytecode_offset: (offsets.procdefs_descriptor >> 16) & 0xFF,
+                },
+                exec_proc_address: transmute(byondcore_base_address + offsets.exec_proc),
+                orig_exec_proc: todo!(),
+                server_tick_address: transmute(byondcore_base_address + offsets.server_tick),
+                orig_server_tick: todo!(),
+                send_maps_address: transmute(byondcore_base_address + offsets.send_maps),
+                orig_send_maps: todo!(),
+                trampoline: todo!(),
+            }
+        }
+    }
+
+    pub fn strings(&self) -> &[String] {
+        unsafe { std::slice::from_raw_parts(self.strings_base_address, *self.strings_len) }
+    }
+
+    fn get_procdef(&self, index: usize) -> Option<ProcdefPointer> {
+        todo!()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,178 @@
-mod proc_definition;
+#![feature(once_cell_try)]
+#![feature(fn_align)]
+mod byond;
+
+use std::{
+    cell::RefCell,
+    ffi::{CString, c_char, c_int},
+    sync::OnceLock,
+};
+
+use tracy_client::Client;
+
+#[cfg(target_os = "windows")]
+use windows::Win32::Foundation::HMODULE;
+use windows::{
+    Win32::System::LibraryLoader::{GetModuleHandleA, GetProcAddress},
+    core::PCSTR,
+};
+
+use crate::byond::{
+    BuildNumber,
+    offsets::{OFFSETS, Offsets},
+};
+
+#[cfg(not(target_pointer_width = "32"))]
+compile_error!("Compiling for non-32bit is not allowed.");
+
+struct Instance {
+    offsets: &'static Offsets,
+    tracy_client: Client,
+}
+
+static EMPTY_STRING: c_char = 0;
+thread_local! {
+    static RETURN_STRING: RefCell<CString> = RefCell::new(CString::default());
+}
+
+static INSTANCE: OnceLock<Instance> = OnceLock::new();
+
+/// SAFETY: This function must only be called via the call()() or call_ext()() procs using the legacy API of a game running using Build Your Own Net Dream (BYOND, https://www.byond.com/).
+/// It relies on reverse engineered internals of the game runtime
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn init(_argc: c_int, _argv: *const *const c_char) -> *const c_char {
+    init_core()
+}
+
+fn byond_return(value: Option<String>) -> *const c_char {
+    match value {
+        None => &EMPTY_STRING,
+        Some(result) if result.is_empty() => &EMPTY_STRING,
+        Some(result) => RETURN_STRING.with(|cell| {
+            // Panicking over an FFI boundary is bad form, so if a NUL ends up
+            // in the result, just truncate.
+            let cstring = match CString::new(result) {
+                Ok(s) => s,
+                Err(e) => {
+                    let (pos, mut vec) = (e.nul_position(), e.into_vec());
+                    vec.truncate(pos);
+                    CString::new(vec).unwrap_or_default()
+                }
+            };
+            cell.replace(cstring);
+            cell.borrow().as_ptr()
+        }),
+    }
+}
+
+fn init_core() -> *const c_char {
+    let mut initialize_attempted = false;
+    match INSTANCE.get_or_try_init(|| {
+        initialize_attempted = true;
+        setup()
+    }) {
+        Ok(_) => if initialize_attempted {
+            "ok"
+        } else {
+            "already initialized"
+        }
+        .as_ptr() as *const c_char,
+        Err(error) => error,
+    }
+}
+
+fn setup() -> Result<Instance, *const c_char> {
+    let (byond_build, byondcore_handle) = match get_byond_build_and_byondcore_handle() {
+        Ok(byond_build) => byond_build,
+        Err(error) => {
+            return Err(if error.is_empty() {
+                &EMPTY_STRING
+            } else {
+                RETURN_STRING.with(|cell| {
+                    // Panicking over an FFI boundary is bad form, so if a NUL ends up
+                    // in the result, just truncate.
+                    let cstring = match CString::new(error) {
+                        Ok(s) => s,
+                        Err(e) => {
+                            let (pos, mut vec) = (e.nul_position(), e.into_vec());
+                            vec.truncate(pos);
+                            CString::new(vec).unwrap_or_default()
+                        }
+                    };
+                    cell.replace(cstring);
+                    cell.borrow().as_ptr()
+                })
+            });
+        }
+    };
+
+    let mut target_offsets = None;
+    for offsets in OFFSETS {
+        if offsets.byond_build == byond_build {
+            target_offsets = Some(offsets)
+        }
+    }
+
+    let offsets = match target_offsets {
+        Some(offsets) => offsets,
+        None => return Err("byond version unsupported".as_ptr() as *const c_char),
+    };
+
+    Ok(Instance {
+        offsets,
+        tracy_client: Client::start(),
+    })
+}
+
+#[cfg(target_os = "windows")]
+fn get_byond_build_and_byondcore_handle() -> Result<(BuildNumber, HMODULE), String> {
+    let byond_dll_name = "byondcore.dll";
+    let byond_dll_cstr = CString::new(byond_dll_name).expect("Why isn't this parsing?");
+    let byond_dll_name_pcstr = PCSTR(byond_dll_cstr.as_bytes().as_ptr());
+
+    // SAFETY: https://learn.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-getmodulehandlea
+    // We rely on BYOND to not unload the .dll
+    let library_load_result = unsafe { GetModuleHandleA(byond_dll_name_pcstr) };
+
+    let byondcore_handle = match library_load_result {
+        Ok(handle) => handle,
+        Err(error) => {
+            return Err(format!(
+                "Unable to find {} handle: {}",
+                byond_dll_name,
+                error.message(),
+            ));
+        }
+    };
+
+    let get_byond_build_name = "?GetByondBuild@ByondLib@@QAEJXZ";
+    let get_byond_build_cstr = CString::new(get_byond_build_name).expect("Why isn't this parsing?");
+    let get_byond_build_pcstr = PCSTR(get_byond_build_cstr.as_bytes().as_ptr());
+
+    // SAFETY: https://learn.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-getprocaddress
+    let get_byond_build_pointer_result =
+        unsafe { GetProcAddress(byondcore_handle, get_byond_build_pcstr) };
+
+    let get_byond_build_farproc_pointer = match get_byond_build_pointer_result {
+        Some(pointer) => pointer,
+        None => {
+            return Err(format!(
+                "Unable to find symbol {} in byondcore.dll!",
+                get_byond_build_name
+            ));
+        }
+    };
+
+    // SAFETY: The symbol specified using get_byond_build_name demangles to the following C++ declaration:
+    // public: long __thiscall ByondLib::GetByondBuild(void)
+    // Reverse engineering shows the "this" pointer is not used in this function
+    unsafe {
+        let get_byond_build: unsafe fn() -> i32 =
+            std::mem::transmute(get_byond_build_farproc_pointer);
+        Ok((get_byond_build(), byondcore_handle))
+    }
+}
+
+fn get_byond_build2() -> Result<BuildNumber, String> {
+    todo!()
+}
